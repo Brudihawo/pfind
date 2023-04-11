4aa89d759a8df8441ff9dd5059ff449a49ea728b
Hawo Höfer <hawo.hoefer98@freenet.de>
2023-04-11 14:55:04 +0200

    add clap for command line args

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,30 @@
+use clap::Parser;
 use num_cpus;
 use pfind::document::{Document, DocumentSet};
 use pfind::lexer::Lexer;
 use std::collections::HashMap;
-use std::env::args;
 use std::path::PathBuf;
 use std::sync::mpsc::channel;
 use std::sync::{Arc, Mutex};
 use std::thread;
 use walkdir::WalkDir;
 
+const DEFAULT_DIRECTORY: &str = ".";
+const DEFAULT_PAGE_THRESH: usize = 25;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about="Query pdfs in a directory via TF-IDF.", long_about=None)]
+struct Args {
+    query: String,
+    #[arg(long, short, help = "Directory to process", default_value_t = DEFAULT_DIRECTORY.to_string())]
+    directory: String,
+    #[arg(long, short, help = "Page number threshold for PDFs to ignore", default_value_t = DEFAULT_PAGE_THRESH)]
+    page_thresh: usize,
+    #[arg(long, short, help = "Verbose output", default_value_t = false)]
+    verbose: bool,
+}
+
 // TODO: make this an optional commandline option
-const PAGE_THRESH: usize = 25;
 
 fn page_counts(text: &str) -> HashMap<String, usize> {
     let mut ret = HashMap::new();
@@ -24,7 +38,7 @@ fn page_counts(text: &str) -> HashMap<String, usize> {
     ret
 }
 
-fn get_document(file_path: &PathBuf) -> Result<Document, ()> {
+fn get_document(file_path: &PathBuf, thresh: usize, verbose: bool) -> Result<Document, ()> {
     let pdf = match poppler::PopplerDocument::new_from_file(file_path, "") {
         Err(err) => {
             eprintln!(
@@ -38,11 +52,13 @@ fn get_document(file_path: &PathBuf) -> Result<Document, ()> {
 
     let num_pages = pdf.get_n_pages();
 
-    if num_pages > PAGE_THRESH {
-        eprintln!(
-            "Skipping {file_path}. Document too large: {num_pages} pages",
-            file_path = file_path.display()
-        );
+    if num_pages > thresh {
+        if verbose {
+            eprintln!(
+                "Skipping {file_path}. Document too large: {num_pages} pages",
+                file_path = file_path.display()
+            );
+        }
         return Err(());
     }
     let mut doc = Document::new(file_path.display().to_string());
@@ -59,7 +75,9 @@ fn get_document(file_path: &PathBuf) -> Result<Document, ()> {
 }
 
 fn main() {
-    let in_path = PathBuf::from(args().nth(1).expect("No path given"));
+    let args = Args::parse();
+
+    let in_path = PathBuf::from(args.directory);
 
     if !in_path.try_exists().unwrap_or(false) {
         eprintln!(
@@ -67,7 +85,9 @@ fn main() {
             path = in_path.display()
         );
     }
-    println!("Reading {path}", path = in_path.display());
+    if args.verbose {
+        println!("Reading {path}", path = in_path.display());
+    }
 
     if in_path.is_file() {
         eprintln!(
@@ -112,11 +132,13 @@ fn main() {
                     }
                 };
                 if let Some(file) = file {
-                    match get_document(&file) {
+                    match get_document(&file, args.page_thresh, args.verbose) {
                         Ok(doc) => tx.send(doc).unwrap(),
                         Err(_) => (),
                     }
-                    println!("{thread_id} {file}", file = file.display());
+                    if args.verbose {
+                        println!("{thread_id} {file}", file = file.display());
+                    }
                 } else {
                     // break if pop returns None. this means the queue is empty
                     break;
@@ -131,20 +153,24 @@ fn main() {
     let docs = DocumentSet::new(rx.try_iter().collect());
 
     let index_duration = (chrono::Local::now() - index_begin).num_milliseconds();
-    println!(
-        "Indexing took {index_duration}s",
-        index_duration = index_duration as f64 / 1000.0
-    );
+    if args.verbose {
+        println!(
+            "Indexing took {index_duration}s",
+            index_duration = index_duration as f64 / 1000.0
+        );
+    }
 
     let before = chrono::Local::now();
-    let mut tf_idfs = docs.tf_idf(Lexer::new("Manipulability tracking").into_iter().collect());
+    let mut tf_idfs = docs.tf_idf(Lexer::new(&args.query).into_iter().collect());
     let mut enumerated = tf_idfs.drain(..).enumerate().collect::<Vec<(usize, f64)>>();
     enumerated.sort_by(|(_, a), (_, b)| b.total_cmp(a));
     let tf_idf_duration = (chrono::Local::now() - before).num_microseconds();
-    println!(
-        "Indexing took {tf_idf_duration} microseconds",
-        tf_idf_duration = tf_idf_duration.unwrap() as f64
-    );
+    if args.verbose {
+        println!(
+            "Indexing took {tf_idf_duration} microseconds",
+            tf_idf_duration = tf_idf_duration.unwrap() as f64
+        );
+    }
     for (idx, val) in enumerated.iter().take(10) {
         println!("{} -> {}: {}", idx, val, docs.get_name(*idx));
     }
